f457fa3b51109b8a4c96202e0807d53ec2e0f5e5
Unknown <lesiak@outlook.com>
2018-08-02 14:41:30 +0200

    Solutions for Exercise8Test

--- company-kata/src/test/java/org/eclipse/collections/companykata/Exercise8Test.java
+++ company-kata/src/test/java/org/eclipse/collections/companykata/Exercise8Test.java
@@ -10,33 +10,19 @@
 
 package org.eclipse.collections.companykata;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.DoubleSupplier;
-import java.util.function.Function;
-import java.util.function.ToDoubleBiFunction;
-import java.util.function.ToDoubleFunction;
-
-import com.google.common.truth.Truth;
-
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Collectors;
 
 import static com.google.common.truth.Truth.assertThat;
 
 /**
  * Below are links to APIs that may be helpful during these exercises.
- *
- * {@link RichIterable#aggregateBy(Function, Function0, Function2)}
- * {@link RichIterable#sumByDouble(Function, DoubleFunction)}
- * {@link RichIterable#flatCollect(Function)}
- * {@link MutableList#select(Predicate)}
- * {@link MutableList#anySatisfy(Predicate)}
- * {@link MutableList#toMap(Function, Function)}
- * {@link RichIterable#asLazy()}
- * {@link MutableList#groupBy(Function)}
- * {@link MutableList#collectDouble(DoubleFunction, MutableDoubleCollection)}
- * {@link MutableDoubleList#max()}
+
  *
  * @see <a href="http://eclipse.github.io/eclipse-collections-kata/company-kata/#/23">Exercise 8 Slides</a>
  */
@@ -45,35 +31,20 @@ public class Exercise8Test extends CompanyDomainForKata
     /**
      * Extra credit. Aggregate the total order values by city.
      *
-     * @see RichIterable#aggregateBy(Function, Function0, Function2)
+     * @see Collectors#summingDouble(ToDoubleFunction)
      */
     @Test
     public void totalOrderValuesByCity()
     {
-        DoubleSupplier zeroValueFactory = () -> 0.0;
-        ToDoubleBiFunction<Double, Customer> aggregator = (result, customer) -> result + customer.getTotalOrderValue();
-
-        Map<String, Double> map = null;
+        Map<String, Double> map = company.getCustomers()
+                .stream()
+                .collect(Collectors.groupingBy(Customer::getCity, Collectors.summingDouble(Customer::getTotalOrderValue)));
         assertThat(map).hasSize(2);
         assertThat(map.get("London")).isWithin(0.0).of(446.25);
         assertThat(map.get("Liphook")).isWithin(0.0).of(857.0);
     }
 
-    /**
-     * Solve the same problem as above using a more specialized API.
-     *
-     * @see RichIterable#sumByDouble(Function, DoubleFunction)
-     */
-    @Test
-    public void totalOrderValuesByCityUsingPrimitiveValues()
-    {
-        Function<Customer, String> cityFunction = Customer::getCity;
-        ToDoubleFunction<Customer> totalOrderValueFunction = Customer::getTotalOrderValue;
-        Map<String, Double> map = null;
-        assertThat(map).hasSize(2);
-        assertThat(map.get("London")).isWithin(0.0).of(446.25);
-        assertThat(map.get("Liphook")).isWithin(0.0).of(857.0);
-    }
+
 
     /**
      * Extra credit. Aggregate the total order values by item.
@@ -83,10 +54,11 @@ public class Exercise8Test extends CompanyDomainForKata
     @Test
     public void totalOrderValuesByItem()
     {
-        DoubleSupplier zeroValueFactory = () -> 0.0;
-        ToDoubleBiFunction<Double, Customer> aggregator = (result, customer) -> result + customer.getTotalOrderValue();
+        Map<String, Double> map = company.getOrders().stream()
+                .flatMap(o -> o.getLineItems().stream())
+                .collect(Collectors.groupingBy(LineItem::getName, Collectors.summingDouble(LineItem::getValue)));
 
-        Map<String, Double> map = null;
+
         assertThat(map).hasSize(12);
         Assert.assertEquals(100.0, map.get("shed"), 0.0);
         Assert.assertEquals(10.5, map.get("cup"), 0.0);
@@ -100,67 +72,77 @@ public class Exercise8Test extends CompanyDomainForKata
     @Test
     public void totalOrderValuesByItemUsingPrimitiveValues()
     {
-        Function<LineItem, String> nameFunction = LineItem::getName;
-        ToDoubleFunction<LineItem> valueFunction = LineItem::getValue;
-        Map<String,Double> map = null;
+        Map<String,Double> map = company.getOrders().stream()
+                .flatMap(o -> o.getLineItems().stream())
+                .collect(Collectors.groupingBy(LineItem::getName, Collectors.summingDouble(LineItem::getValue)));
+
         assertThat(map).hasSize(12);
         Assert.assertEquals(100.0, map.get("shed"), 0.0);
         Assert.assertEquals(10.5, map.get("cup"), 0.0);
     }
 
-//    /**
-//     * Extra credit. Find all customers' line item values greater than 7.5 and sort them by highest to lowest price.
-//     */
-//    @Test
-//    public void sortedOrders()
-//    {
-//        MutableSortedBag<Double> orderedPrices = null;
-//
-//        MutableSortedBag<Double> expectedPrices = SortedBags.mutable.with(
-//                Collections.reverseOrder(), 500.0, 150.0, 120.0, 75.0, 50.0, 50.0, 12.5);
-//        Verify.assertSortedBagsEqual(expectedPrices, orderedPrices);
-//    }
-//
-//    /**
-//     * Extra credit. Figure out which customers ordered saucers (in any of their orders).
-//     */
-//    @Test
-//    public void whoOrderedSaucers()
-//    {
-//        MutableList<Customer> customersWithSaucers = null;
-//        Verify.assertSize("customers with saucers", 2, customersWithSaucers);
-//    }
-//
-//    /**
-//     * Extra credit. Look into the {@link MutableList#toMap(Function, Function)} method.
-//     */
-//    @Test
-//    public void ordersByCustomerUsingAsMap()
-//    {
-//        MutableMap<String, MutableList<Order>> customerNameToOrders =
-//                this.company.getCustomers().toMap(null, null);
-//
-//        Assert.assertNotNull("customer name to orders", customerNameToOrders);
-//        Verify.assertSize("customer names", 3, customerNameToOrders);
-//        MutableList<Order> ordersForBill = customerNameToOrders.get("Bill");
-//        Verify.assertSize("Bill orders", 3, ordersForBill);
-//    }
-//
-//    /**
-//     * Extra credit. Create a multimap where the values are customers and the key is the price of
-//     * the most expensive item that the customer ordered.
-//     */
-//    @Test
-//    public void mostExpensiveItem()
-//    {
-//        MutableListMultimap<Double, Customer> multimap = null;
-//        Verify.assertSize(3, multimap);
-//        Verify.assertSize(2, multimap.keysView());
-//        Assert.assertEquals(
-//                Lists.mutable.with(
-//                        this.company.getCustomerNamed("Fred"),
-//                        this.company.getCustomerNamed("Bill")),
-//                multimap.get(50.0));
-//    }
-//
+    /**
+     * Extra credit. Find all customers' line item values greater than 7.5 and sort them by highest to lowest price.
+     */
+    @Test
+    public void sortedOrders()
+    {
+        List<Double> orderedPrices = company.getOrders().stream()
+                .flatMap(o -> o.getLineItems().stream())
+                .map(LineItem::getValue)
+                .filter(v -> v > 7.5)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        assertThat(orderedPrices).containsExactly(500.0, 150.0, 120.0, 75.0, 50.0, 50.0, 12.5).inOrder();
+    }
+
+    /**
+     * Extra credit. Figure out which customers ordered saucers (in any of their orders).
+     */
+    @Test
+    public void whoOrderedSaucers()
+    {
+        List<Customer> customersWithSaucers = company.getCustomers().stream()
+                .filter(c -> c.getOrders().stream().flatMap(o -> o.getLineItems().stream()).anyMatch(li -> li.getName().equals("saucer")))
+                .collect(Collectors.toList());
+        assertThat(customersWithSaucers).hasSize(2);
+    }
+
+    /**
+     * Extra credit. Look into the {@link MutableList#toMap(Function, Function)} method.
+     */
+    @Test
+    public void ordersByCustomerUsingAsMap()
+    {
+       Map<String, List<Order>> customerNameToOrders =
+                this.company.getCustomers().stream().collect(Collectors.toMap(Customer::getName, Customer::getOrders));
+
+        assertThat(customerNameToOrders).isNotNull();
+        assertThat(customerNameToOrders).hasSize(3);
+        List<Order> ordersForBill = customerNameToOrders.get("Bill");
+        assertThat(ordersForBill).hasSize(3);
+    }
+
+    /**
+     * Extra credit. Create a multimap where the values are customers and the key is the price of
+     * the most expensive item that the customer ordered.
+     */
+    @Test
+    public void mostExpensiveItem()
+    {
+        Map<Double, List<Customer>> multimap = company.getCustomers().stream()
+                .collect(Collectors.groupingBy(c -> c.getOrders().stream().flatMap(o -> o.getLineItems().stream())
+                        .mapToDouble(LineItem::getValue)
+                        .max().orElse(0.0) ));
+
+        assertThat(multimap).hasSize(2);
+        assertThat(multimap.values().stream().flatMap(Collection::stream).collect(Collectors.toList())).hasSize(3);
+
+        assertThat(multimap.get(50.0)).containsExactly(
+                company.getCustomerNamed("Fred"),
+                company.getCustomerNamed("Bill")).inOrder();
+
+    }
+
 }
